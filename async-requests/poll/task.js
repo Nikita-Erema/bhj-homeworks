c5d9469de2f8d5ac0e2c5ff5ab550e83c35c5029
Archick <k0bana@mail.ru>
2025-07-02 12:10:10 +0300

    Second commit

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,19 +1,20 @@
 const xhr = new XMLHttpRequest;
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
 xhr.send();
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-        console.log(JSON.parse(xhr.response));
+xhr.upload.onload = () => {
+    if (xhr.status === 200) {
         document.getElementById('poll__title').insertAdjacentText('afterend', JSON.parse(xhr.response).data.title);
         for(let i = 0; i < JSON.parse(xhr.response).data.answers.length; i++) {
             document.getElementById('poll__answers').insertAdjacentHTML('beforeend', `<button class="poll__answer">
                 ${JSON.parse(xhr.response).data.answers[i]}
                 </button>`)
-        }
-        document.querySelectorAll('.poll__answer').forEach((e) => {
-            e.addEventListener('click', () => {
-                alert('Спасибо, ваш голос засчитан!')
+            }
+            document.querySelectorAll('.poll__answer').forEach((e) => {
+                e.addEventListener('click', () => {
+                    alert('Спасибо, ваш голос засчитан!')
+                })
             })
-        })
-    }
-}
+        } else if (xhr.status === 404) {
+            console.error(404);
+        }
+    } 
--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,18 +1,23 @@
 const form = document.querySelector('#form');
 const formFile = document.querySelector('#file')
-console.log(form);
+const progressBar = document.getElementById('progress');
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
     const formData = new FormData(form)
-    console.log(Object.fromEntries(formData))
     let xhr = new XMLHttpRequest;
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload')
-    xhr.upload.onload = () => {
-        if(xhr.status === 200) {
-            console.log('файл загружен');
-        } else {
-            console.log(formData)
-            console.log('ошибка');
+    xhr.send(formData);
+    xhr.upload.loadstart = () => {
+        progressBar.value = 0
+    }
+    xhr.upload.onprogress = (e) => {
+        progressBar.value = e.loaded / e.total
+    }
+    xhr.onloadend = () => {
+        if (xhr.status == 200) {
+            console.log('YEEEEES')
+        } else if (xhr.status == 404){
+            
         }
     }
-    xhr.send(formData);
 })
